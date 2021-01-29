2861b390bcce67cac4557249c341f12c568b4fb2
RAVITEJA-KOMPALLI <77142403+RAVITEJA-KOMPALLI@users.noreply.github.com>
2021-01-29 17:10:09 +0530

    Update DataBaseCreation.py

--- DataBaseCreation.py
+++ DataBaseCreation.py
@@ -2,7 +2,7 @@ import mysql.connector as m
 mydb = m.connect(host = "localhost",user = "root", password = "Raviteja@01")
 mycursor = mydb.cursor()
 mycursor.execute("create database studentdb")
-# you should make the aboue statement(  mycursor.execute("create database studentdb")  ) as comments after you run it once (WHY? we cannot create the same database multiple times)
+# you should make the aboue statement(  mycursor.execute("create database studentdb")  ) as comments after you run it once ( WHY? Ans: we cannot create the same database multiple times)
 mycursor.execute("show databases")
 for i in mycursor:
     print(i)
